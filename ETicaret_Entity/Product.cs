a70f8ada5a24f2a1c1e8db1d43e6247f04ed5f94
Şekibe EKŞİ <83080351+eksisekibe@users.noreply.github.com>
2022-05-22 12:07:09 +0300

    Add files via upload
    Entity Katmanı

--- ETicaret_Entity/Product.cs
+++ ETicaret_Entity/Product.cs
@@ -11,9 +11,12 @@ namespace ETicaret_Entity
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double? Price { get; set; }
+        public string Color  { get; set; }
+        public string Size { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public bool IsApproved { get; set; }
 
+        
     }
 }
